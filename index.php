d90e5cadcf32e2325191b6e206cafc43f4ff7ef1
jawuermkr <guitarro92@hotmail.com>
2023-03-16 22:54:18 -0500

    avance landing

---
+++ index.php
@@ -0,0 +1,108 @@
+<html lang="es">
+  <head>
+    <title>3c Hotels | 13 años de aniversario</title>
+    <link href="css-ani/estilos.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+    <img src="media/banner.jpg" width="100%"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-center p-5 mt-5 mb-5"><b class="rojo-text">¡Estamos de aniversario!</b><br/>
+          Celebra con nosotros 13 años de sueños cumplidos.</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="azul">
+      <div class="row">
+        <div class="col-md-6 p-5">
+          <div class="card p-3">
+            <video autoplay controls loop>
+              <source src="media/videoAni.mp4" type="video/mp4"/>
+            </video>
+          </div>
+        </div>
+        <div class="col-md-6 abs-center">
+          <h3 class="p-5">Gracias por un año más compartiendo y siendo parte de esta gran historia.</h3>
+        </div>
+      </div> 
+    </div>
+    <img src="media/bannerPromo.jpg" width="100%"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+        <h2 class="text-center p-5 mt-5 mb-5"><b class="rojo-text">Son 13 𝗮ñ𝗼𝘀 que 𝗾𝘂𝗲𝗿𝗲𝗺𝗼𝘀 celebrar junto a ti</b>, por eso te traemos la estadía perfecta, <b class="verde-text">del 1 al 31 de 𝗺𝗮𝗿𝘇𝗼</b> disfruta de nuestras promos.
+        </h2>
+        </div>
+      </div> 
+    </div>
+    <div class="verde">
+      <div class="row p-5">
+        <div class="col-md-6 p-5">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="media/promo1.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo2.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo3.jpg" alt="Third slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo4.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-6 p-5">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="media/promo5.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo6.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo7.jpg" alt="Third slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo8.jpg" alt="Third slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="media/promo9.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-12 text-center">
+          <button class="btn btn-light">¡Reserva Ahora!</button>
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
